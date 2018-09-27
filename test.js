debfc4956195f5be240a47d437753d7f490306e0
Tim Gibson <tg3843@my.open.ac.uk>
2018-09-27 21:07:22 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-test
-<script>
+
 var a = prompt('TEST');
-document.write("po");
-</script>
+document.write("this is a test");
+document.write(a);
+var a = 1;
+var b = 2;
+var c = a + b;
+console.log(c);
+
